962a63120fd383debed4b850e9b4c684a61f40b3
colour-me-bibi <melancholicredux.co@gmail.com>
2019-12-13 11:24:56 -0800

    Initital commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# HardlyLearnin
+
+This is an application to query a directory of docx and txt files
---
+++ main.py
@@ -0,0 +1,8 @@
+import tkinter
+import rx
+import os
+import shutil
+
+
+if __name__ == '__main__':
+    pass
